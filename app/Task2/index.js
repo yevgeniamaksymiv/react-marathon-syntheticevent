f28957ec4962ca40ff0490d91d248b8d34969829
yevgeniamaksymiv <evgeniamaksymiv@gmail.com>
2021-12-29 16:01:40 +0200

    added event mouse hover

--- app/Task2/index.js
+++ app/Task2/index.js
@@ -1,29 +1,46 @@
 import React, { Component } from "react";
 
 export default class Task2 extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            list: [
-                {
-                    id: 1,
-                    text: "hidden text 1"
-                },
-                {
-                    id: 2,
-                    text: "hidden text 2"
-                },
-                {
-                    id: 3,
-                    text: "hidden text 3"
-                },
-            ],
-            text: ''
-        };
-    }
+    this.state = {
+      list: [
+        {
+          id: 1,
+          text: "hidden text 1",
+        },
+        {
+          id: 2,
+          text: "hidden text 2",
+        },
+        {
+          id: 3,
+          text: "hidden text 3",
+        },
+      ],
+      text: "",
+    };
+  }
 
-    render() {
-        return null;
-    }
+  render() {
+    const { list, text } = this.state;
+    return (
+      <div>
+        <ul>
+          {list.map((item) => (
+            <li
+                  key={item.id}
+                  className="element"
+                  onMouseOver={() => { this.setState({ text: item.text }) }}
+                  onMouseOut={() => { this.setState({ text: '' }) }}
+            >
+              id - {item.id}
+            </li>
+          ))}
+        </ul>
+        <div className="text">{text}</div>
+      </div>
+    );
+  }
 }
